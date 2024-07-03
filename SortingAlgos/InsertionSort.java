35ee208e4de0b7838e76d224578125ca8a97c646
Jayasimman10 <91558509+Jayasimman10@users.noreply.github.com>
2024-07-03 19:16:38 +0530

    update

--- SortingAlgos/InsertionSort.java
+++ SortingAlgos/InsertionSort.java
@@ -2,8 +2,8 @@ package SortingAlgos;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] arr = {9, 14, 15, 12, 6, 8, 13};
-        int len = arr.length;
+        // int[] arr = {9, 14, 15, 12, 6, 8, 13};
+        // int len = arr.length;
 
         
     }
